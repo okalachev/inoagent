867a1c4202677284c8c4881e8c9b4fbaf2c31469
Oleg Kalachev <okalachev@gmail.com>
2021-10-24 10:35:32 +0300

    Initial

---
+++ README.md
@@ -0,0 +1,17 @@
+# inoagent
+
+Пользовательский скрипт (userscript), скрывающий плашки об иностранных агентах на сайтах СМИ:
+
+* «Медуза»
+* «Дождь»
+* «Медиазона»
+* «Росбалт»
+* «Важные Истории»
+* «Первое Антикоррупционное СМИ»
+
+<img src="inoagent.png" width=500>
+
+## Установка
+
+* Установите расширение для пользовательских скриптов: [Tampermonkey](https://www.tampermonkey.net) или [аналогичное](https://openuserjs.org/about/Userscript-Beginners-HOWTO#how-do-i-get-going-).
+* Установите скрипт *inoagent*: https://openuserjs.org/scripts/chev/inoagent.
---
+++ inoagent.user.js
@@ -0,0 +1,40 @@
+// ==UserScript==
+// @name            inoagent
+// @version         1.0
+// @description     Hide foreign agent banners on popular media websites
+// @description:ru  Скрыть баннер об иностранном агенте на сайтах популярных СМИ
+// @author          Oleg Kalachev
+// @license         MIT
+// @match           https://www.rosbalt.ru/*
+// @match           https://meduza.io/*
+// @match           https://tvrain.ru/*
+// @match           https://istories.media/*
+// @match           https://pasmi.ru/*
+// @match           https://zona.media/*
+// @grant           GM_addStyle
+// @run-at          document-start
+// @updateURL       https://openuserjs.org/meta/chev/inoagent.meta.js
+// @downloadURL     https://openuserjs.org/install/chev/inoagent.user.js
+// ==/UserScript==
+
+// Registry: https://minjust.gov.ru/ru/documents/7755/
+
+if (location.host == 'meduza.io') {
+	GM_addStyle('#div-gpt-ad { display: none !important }');
+
+} else if (location.host == 'tvrain.ru') {
+	GM_addStyle('.menu3__foreign-agent-wrapper { display: none !important }');
+	GM_addStyle('.document-head__title_foreign_agent { display: none !important }');
+
+} else if (location.host == 'zona.media') {
+	GM_addStyle('.mz-agent-banner { display: none !important }');
+
+} else if (location.host == 'www.rosbalt.ru') {
+	GM_addStyle('.rkngov { display: none !important }');
+
+} else if (location.host == 'istories.media') {
+	GM_addStyle('.ForeignAgentBanner { display: none !important }');
+
+} else if (location.host == 'pasmi.ru') {
+	GM_addStyle('.content .excerpt:first-of-type { display: none !important }');
+}
